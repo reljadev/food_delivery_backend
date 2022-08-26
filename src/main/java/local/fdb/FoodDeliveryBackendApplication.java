61f98457199a66d549142fa3eba94ba2e1fb6a43
reljadev <milan54321@hotmail.com>
2022-08-26 19:41:38 +0200

    Added SSL certificate

--- src/main/java/local/fdb/FoodDeliveryBackendApplication.java
+++ src/main/java/local/fdb/FoodDeliveryBackendApplication.java
@@ -3,6 +3,7 @@ package local.fdb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.SpringVersion;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
